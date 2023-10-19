ec7d91ae16e1af5b58e4040b23659e86bbc2229b
Moamen <msh@luftborn.com>
2023-10-19 18:09:27 +0300

    added unit test Should_Create_New_Url_If_Does_Not_Exist

--- Moamen.SiderProjects.Application.Tests/Features/Urls/Commands/UpsertUrlCommandHandlerTests.cs
+++ Moamen.SiderProjects.Application.Tests/Features/Urls/Commands/UpsertUrlCommandHandlerTests.cs
@@ -48,5 +48,49 @@ namespace Moamen.SiderProjects.Application.Tests.Features.Urls.Commands
 			dbContextMock
 				.Verify(d => d.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Never);
 		}
+
+		[Fact]
+		public async Task Should_Create_New_Url_If_Does_Not_Exist()
+		{
+			//arrange
+			var request = new UpsertUrlCommand("localhost:7541/urls/rbwjt",
+				"https://www.google.com/images?name=aywa.jpg", "hashedValue");
+
+			var dbContextMock = new Mock<IUrlsDbContext>();
+			dbContextMock.Setup(x => x.Urls)
+				.ReturnsDbSet(new List<Url>());
+
+			dbContextMock
+				.Setup(c => c.SaveChangesAsync(It.IsAny<CancellationToken>()))
+				.ReturnsAsync(1);
+
+		var mapperMock = new Mock<IMapper>();
+			mapperMock.Setup(m => m.Map<Url>(request))
+				.Returns(new Url
+				{
+					ShortUrl = request.ShortUrl,
+					OriginalUrl = request.OriginalUrl,
+					OriginalUrlHashed = request.OriginalUrlHash
+				});
+
+			mapperMock.Setup(m => m.Map<UrlDto>(It.IsAny<Url>()))
+				.Returns(new UrlDto
+				{
+					ShortUrl = request.ShortUrl
+				});
+
+			var handler = new UpsertUrlCommandHandler(dbContextMock.Object, mapperMock.Object);
+
+			//act
+			var response = await handler.Handle(request, CancellationToken.None);
+
+			//assert
+			Assert.NotNull(response);
+			Assert.Equal(response.ShortUrl, request.ShortUrl);
+			dbContextMock
+				.Verify(d => d.SaveChangesAsync(It.IsAny<CancellationToken>()), Times.Once);
+		}
+
+
 	}
 }
