64ea53525b3c7bd96a175d153a88ba50553f164d
Moamen <msh@luftborn.com>
2023-10-19 16:45:21 +0300

    Add project files.

---
+++ Moamen.SiderProjects.UrlSHortener/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Moamen.SiderProjects.UrlSHortener.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
