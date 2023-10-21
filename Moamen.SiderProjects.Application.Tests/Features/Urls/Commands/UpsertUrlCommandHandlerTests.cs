25ae4226d26bcaca35c205853e5aafe9d0b3d84e
Moamen <msh@luftborn.com>
2023-10-21 07:54:36 +0300

    refactored naming for originalHashedValue

--- Moamen.SiderProjects.Application.Tests/Features/Urls/Commands/UpsertUrlCommandHandlerTests.cs
+++ Moamen.SiderProjects.Application.Tests/Features/Urls/Commands/UpsertUrlCommandHandlerTests.cs
@@ -21,7 +21,7 @@ namespace Moamen.SiderProjects.Application.Tests.Features.Urls.Commands
 
 			var existingUrl = new Url()
 			{
-				Hash = request.OriginalUrlHash,
+				Hash = request.Hash,
 				ShortUrl = request.ShortUrl,
 				OriginalUrl = request.OriginalUrl
 			};
@@ -72,7 +72,7 @@ namespace Moamen.SiderProjects.Application.Tests.Features.Urls.Commands
 				{
 					ShortUrl = request.ShortUrl,
 					OriginalUrl = request.OriginalUrl,
-					Hash = request.OriginalUrlHash
+					Hash = request.Hash
 				});
 
 			mapperMock.Setup(m => m.Map<UrlDto>(It.IsAny<Url>()))
--- Moamen.SiderProjects.Application/Features/Urls/Commands/UpsertUrlCommand.cs
+++ Moamen.SiderProjects.Application/Features/Urls/Commands/UpsertUrlCommand.cs
@@ -8,7 +8,7 @@ using Moamen.SiderProjects.Domain;
 
 namespace Moamen.SiderProjects.Application.Features.Urls.Commands
 {
-	public record UpsertUrlCommand(string ShortUrl, string OriginalUrl, string OriginalUrlHash) : IRequest<UrlDto>;
+	public record UpsertUrlCommand(string ShortUrl, string OriginalUrl, string Hash) : IRequest<UrlDto>;
 
 	public class UpsertUrlCommandHandler : IRequestHandler<UpsertUrlCommand, UrlDto>
 	{
@@ -27,7 +27,7 @@ namespace Moamen.SiderProjects.Application.Features.Urls.Commands
 
 		public async Task<UrlDto> Handle(UpsertUrlCommand request, CancellationToken cancellationToken)
 		{
-			var urlByHash = await GetUrlByHashedUrlAsync(request.OriginalUrlHash, cancellationToken);
+			var urlByHash = await GetUrlByHashAsync(request.Hash, cancellationToken);
 
 			UrlDto result = null;
 
@@ -40,10 +40,10 @@ namespace Moamen.SiderProjects.Application.Features.Urls.Commands
 			return result;
 		}
 
-		private async Task<Url> GetUrlByHashedUrlAsync(string hashedOriginalUrl, CancellationToken cancellationToken)
+		private async Task<Url> GetUrlByHashAsync(string hash, CancellationToken cancellationToken)
 		{
 			return await _urlsDbContext.Urls
-				.FirstOrDefaultAsync(url => url.Hash == hashedOriginalUrl, cancellationToken);
+				.FirstOrDefaultAsync(url => url.Hash == hash, cancellationToken);
 		}
 
 		private async Task<UrlDto> SaveUrlToDatabaseAsync(UpsertUrlCommand request, CancellationToken cancellationToken)
